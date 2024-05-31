6d09585c8cbb9cf1ca902dba15aebf218c9ec539
Hetso <datacener23491248@gmail.com>
2024-05-31 05:02:10 +0500

    пофиксил ошибки

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
 	// Обновленная настройка CORS
 	app.enableCors({
-		origin: 'https://frontend-english-school-8lzq71krq-xhetsos-projects.vercel.app/', // или использовать true для разрешения всех источников
+		origin: 'https://frontend-english-school-1prn-gd1lau943-xhetsos-projects.vercel.app', // или использовать true для разрешения всех источников
 		credentials: true,
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		allowedHeaders: 'Content-Type, Accept, Authorization', // Добавить Authorization
